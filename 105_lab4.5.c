f25c968cef9dfe2b61540ef8fe08bd7add714fc1
supanat <supanat.ap@gmail.com>
2021-03-01 17:13:46 +0700

    Update 105_lab4.5.c

--- 105_lab4.5.c
+++ 105_lab4.5.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
+#include<string.h>
 int main()
-{
+{   int n,i,j;
+    char a[50];
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",a);
+
+        if()
+
+
+    }
 
 
 
