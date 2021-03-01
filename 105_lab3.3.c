36d4380745226627186caf8bef01b0f31309148f
supanat <supanat.ap@gmail.com>
2021-03-01 17:00:34 +0700

    Update 105_lab3.3.c

--- 105_lab3.3.c
+++ 105_lab3.3.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
 #include<string.h>
 int main()
-{   char a[200];
+{   char a[200],an[200];
     int b;
 
     scanf("%s",a);
     b=strlen(a);
-    printf("%d",b);
+    //printf("%d",b);
+    while(i<b)
+    {
+        if(a[i-1]!=a[i])
+        {
+            an[i]=a[i];
+        }
+        i++;
+
+
+
+    }
 
 
 
