958fc6a01ac8cc13ab8760d7e545a741606b9628
supanat <supanat.ap@gmail.com>
2021-03-01 19:22:46 +0700

    a

--- 105_lab4.1.c
+++ 105_lab4.1.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 int main()
-{      int n,a,b,c;
+{      int n,a,b,c,i;
 
     scanf("%d",&n);
-    int sum[n];
+    int sum[n],answer[2000]={};
     for(i=0;i<n;i++)
     {
         scanf("%d %d %d",&a,&b,&c);
-        sum[i]=a+b+c;
+        sum[i]=(a*4)+(b*2)+c;
     }
 
-      for(i=0;i<n;i++)
+    for(i=0;i<n;i++)
     {
-        printf("%d,",sum[i]);
+        answer[i]=sum[i]+sum[i+1]+sum[i+2];
+    }
+    for()
+    {
+
     }
 
 
