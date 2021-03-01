4a480759e8348bf63f087ab0b77aa157733b3aed
supanat <supanat.ap@gmail.com>
2021-03-01 19:37:23 +0700

    a

--- 105_lab4.1.c
+++ 105_lab4.1.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
-{      int n,a,b,c,i;
+{      int n,a,b,c,i,max=0,min=10;
 
     scanf("%d",&n);
     int sum[n],answer[2000]={};
@@ -10,15 +10,19 @@ int main()
         sum[i]=(a*4)+(b*2)+c;
     }
 
-    for(i=0;i<n;i++)
+    for(i=0;i<n/3;i++)
     {
         answer[i]=sum[i]+sum[i+1]+sum[i+2];
     }
-    for()
+
+    for(i=0;i<n/3;i++)
     {
-
+        if(answer[i]>max)
+            max=i;
+        else if(answer[i]<min)
+            min=i;
     }
-
+    printf("%d\n%d",max+1,min+1);
 
 
 
