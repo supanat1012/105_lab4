fc1813950577fc2f8650034197549248879a3975
supanat <supanat.ap@gmail.com>
2021-03-01 19:10:13 +0700

    Update 105_lab4.1.c

--- 105_lab4.1.c
+++ 105_lab4.1.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 int main()
-{      int
+{      int n,a,b,c;
 
+    scanf("%d",&n);
+    int sum[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d %d",&a,&b,&c);
+        sum[i]=a+b+c;
+    }
+
+      for(i=0;i<n;i++)
+    {
+        printf("%d,",sum[i]);
+    }
 
 
 
